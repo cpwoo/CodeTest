1b72a1ed4956b5bd4e93403424823d92feed5cef
cpwoo <world9969@khu.ac.kr>
2023-02-17 00:23:01 +0900

    Update boj 1107

---
+++ boj/bruteForce/1107.py
@@ -0,0 +1,20 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+target = int(input())
+n = int(input())
+broken = list(map(int, input().split()))
+
+cnt = abs(100-target)
+
+for nums in range(100_001):
+    nums = str(nums)
+
+    for i in range(len(nums)):
+        if int(nums[i]) in broken:
+            break
+
+        elif i == len(nums)-1:
+            cnt = min(cnt, abs(int(nums)-target)+len(nums))
+
+print(cnt)
