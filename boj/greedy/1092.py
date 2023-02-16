e65edc3747dbff2f07306b4fcabbbb86942c8950
cpwoo <world9969@khu.ac.kr>
2023-02-16 23:54:43 +0900

    Update boj 1092

---
+++ boj/greedy/1092.py
@@ -0,0 +1,23 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+crane = sorted(list(map(int, input().split())), reverse=True)
+m = int(input())
+box = sorted(list(map(int, input().split())), reverse=True)
+
+if box[0] > crane[0]:
+    print(-1)
+    exit()
+else:
+    time = 0
+    while box:
+        if not box:
+            break
+        for c in crane:
+            for b in box:
+                if c >= b:
+                    box.remove(b)
+                    break
+        time += 1
+    print(time)
