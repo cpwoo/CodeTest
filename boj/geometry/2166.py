6da51313e3adcb5e920d4e220633741d12196034
cpwoo <world9969@khu.ac.kr>
2023-02-17 15:59:06 +0900

    Update boj 2166

---
+++ boj/geometry/2166.py
@@ -0,0 +1,20 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+x, y = [], []
+n = int(input())
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    x.append(a)
+    y.append(b)
+
+x.append(x[0])
+y.append(y[0])
+
+xr, yr = 0, 0
+for i in range(n):
+    xr += x[i] * y[i+1]
+    yr += y[i] * x[i+1]
+
+print(round(abs((xr-yr)/2), 1))
