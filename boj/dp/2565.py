54d9d5f30910f4d6a18c6ecd5204e1359808452e
cpwoo <world9969@khu.ac.kr>
2023-02-18 22:35:26 +0900

    Update boj 2565

---
+++ boj/dp/2565.py
@@ -0,0 +1,14 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+arr = sorted([list(map(int, input().split())) for _ in range(n)])
+
+dp = [1]*n
+
+for i in range(n):
+    for j in range(i):
+        if arr[i][1] > arr[j][1]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n-max(dp))
