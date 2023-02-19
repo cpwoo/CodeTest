775eb82c0327096e89ca5472ad83a1a548e07a26
cpwoo <world9969@khu.ac.kr>
2023-02-19 18:24:57 +0900

    Update boj 5557

---
+++ boj/dp/5557.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [[0]*21 for _ in range(n)]
+dp[0][arr[0]] = 1
+target = arr[-1]
+
+for i in range(1, n-1):
+    for j in range(21):
+        if dp[i-1][j]:
+            if j+arr[i] <= 20:
+                dp[i][j+arr[i]] += dp[i-1][j]
+            if j-arr[i] >= 0:
+                dp[i][j-arr[i]] += dp[i-1][j]
+
+print(dp[n-2][target])
