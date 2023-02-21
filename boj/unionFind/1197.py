b87889e6dd2cc77b0be273cbd88163ea45a9d171
cpwoo <world9969@khu.ac.kr>
2023-02-21 17:45:27 +0900

    Update boj 1197

---
+++ boj/unionFind/1197.py
@@ -0,0 +1,31 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a, b):
+    a, b = find(a), find(b)
+    parent[max(a, b)] = min(a, b)
+
+
+V, S = map(int, input().split())
+
+edge = []
+for _ in range(S):
+    a, b, w = map(int, input().split())
+    edge.append((w, a, b))
+
+edge.sort(key=lambda x: x[0])
+
+parent = list(range(V+1))
+
+total = 0
+for w, s, e in edge:
+    if find(s) != find(e):
+        union(s, e)
+        total += w
+
+print(total)
