1d852a2971ede8d3dd3571f92ed9ea0e0adae1db
cpwoo <world9969@khu.ac.kr>
2023-02-21 18:17:59 +0900

    Update boj 1405

---
+++ boj/graph/1405.py
@@ -0,0 +1,31 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+dx, dy = [1,-1,0,0], [0,0,1,-1]
+
+def dfs(x,y,percent,cnt):
+    global answer
+    if cnt == N:
+        answer += percent
+        return
+    now_percent = percent
+    graph[x][y] = 1
+    for i in range(4):
+        nx, ny = x+dx[i], y+dy[i]
+        if (0 <= nx < (2*N+1)) and (0 <= ny < (2*N+1)):
+            if graph[nx][ny] == 1:
+                continue
+            else:
+                dfs(nx,ny,now_percent*(percent_data[i]/100),cnt+1)
+                graph[nx][ny] = 0
+
+
+N,e,w,s,n = map(int,input().split())
+
+percent_data= [e,w,s,n]
+graph = [[0] * (2*N+1) for _ in range(2*N+1)]
+answer = 0
+
+dfs(N,N,1,0)
+
+print(answer)
