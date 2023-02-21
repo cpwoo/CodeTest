3c3ba49df1e9178bc8c36254e089473843eefa89
cpwoo <world9969@khu.ac.kr>
2023-02-21 18:10:49 +0900

    Update boj 1339

---
+++ boj/greedy/1339.py
@@ -0,0 +1,26 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+words = [input() for _ in range(int(input()))]
+
+d = {}
+
+for word in words:
+    digit = len(word)-1
+    for w in word:
+        if w in dict:
+            d[w] += pow(10, digit)
+        else:
+            d[w] = pow(10, digit)
+        digit -= 1
+
+d = sorted(d.values(), reverse=True)
+
+result = 0
+m = 9
+
+for value in d:
+    result += value * m
+    m -= 1
+
+print(result)
