ef8f12acdf0fa3a89b4708177a93fd91b13bf118
cpwoo <world9969@khu.ac.kr>
2023-02-23 17:51:54 +0900

    Update boj 2285

---
+++ boj/greedy/2285.py
@@ -0,0 +1,22 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n = int(input())
+lst = []
+s = 0
+for _ in range(n):
+    x, a = map(int, input().split())
+    lst.append([x, a])
+    s += a
+
+lst.sort()
+
+cnt = 0
+pos = 0
+for i in range(n):
+    cnt += lst[i][1]
+    if cnt >= s/2:
+        pos = lst[i][0]
+        break
+        
+print(pos)
