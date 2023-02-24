55bfab12ba7c480e1d96c90e50af26a0f501442f
cpwoo <world9969@khu.ac.kr>
2023-02-24 17:42:52 +0900

    Update boj 11404

---
+++ boj/shortestPath/11404.py
@@ -0,0 +1,27 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+INF = int(1e9)
+
+n, m = int(input()), int(input())
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, d = map(int, input().split())
+    graph[a][b] = min(graph[a][b], d)
+    
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i == j:
+                graph[i][j] = 0
+            else:
+                graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
