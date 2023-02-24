08910e25ada2dba7a5efc0f43e19fd209d4ac71e
cpwoo <world9969@khu.ac.kr>
2023-02-24 18:11:40 +0900

    Update boj 13302

---
+++ boj/dp/13302.py
@@ -0,0 +1,36 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+INF = sys.maxsize
+
+N, K = map(int, input().split())
+if K == 0:
+    holidays = set()
+else:
+    holidays = set(map(int, input().split()))
+
+dp = [[INF for _ in range(110)] for _ in range(110)]  
+dp[0][0] = 0
+
+for i in range(N+1):
+    for j in range(40):
+        if dp[i][j] == INF:
+            continue
+
+        result = dp[i][j]
+
+        if i+1 in holidays:
+            dp[i+1][j] = min(result, dp[i+1][j])
+        
+        if j >= 3 :
+            dp[i+1][j-3] = min(dp[i+1][j-3], result)
+        
+        dp[i+1][j] = min(result+10000, dp[i+1][j])
+        
+        for k in range(1, 4):
+            dp[i+k][j+1] = min(result+25000, dp[i+k][j+1])
+        
+        for k in range(1, 6):
+            dp[i+k][j+2] = min(result+37000, dp[i+k][j+2])
+
+print(min(dp[N]))
