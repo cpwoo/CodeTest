09d640628830b2aae5dc8a0756a8e68d2c26bcb4
cpwoo <world9969@khu.ac.kr>
2023-03-11 17:53:15 +0900

    Update boj 2836

---
+++ boj/sorting/2836.py
@@ -0,0 +1,31 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+INF = int(1e9)
+
+n, m = map(int, input().split())
+
+arr = []
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a > b:
+        arr.append([a, b])
+
+arr.sort(reverse=True)
+
+ret = m
+start, end = -1, -1
+for i in range(len(arr)):
+    From, To = arr[i]
+    if (start, end) == (-1, -1):
+        start, end = From, To
+    else:
+        if From >= end:
+            end = min(end, To)
+        else:
+            ret += 2*(start-end)
+            start, end = From, To
+
+ret += 2*(start-end)
+
+print(ret)
