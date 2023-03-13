aaae15ab4bf93bceaa15d5a1cff4c83043bc5b91
cpwoo <world9969@khu.ac.kr>
2023-03-13 14:08:28 +0900

    Update boj 20542

---
+++ boj/dp/20542.py
@@ -0,0 +1,16 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+n, m = map(int, input().split())
+w1, w2 = input(), input()
+
+dp = [[i]+[0]*m for i in range(n+1)]
+dp[0] = list(range(m+1))
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])+1
+        if w1[i-1] == w2[j-1] or (w1[i-1] == 'v' and w2[j-1] == 'w') or (w1[i-1] == 'i' and w2[j-1] in ['j', 'l']):
+            dp[i][j] = dp[i-1][j-1]
+
+print(dp[n][m])
