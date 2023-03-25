c094d731f880238dbb9869cba95eeefcb7a6a302
cpwoo <world9969@khu.ac.kr>
2023-03-25 16:23:45 +0900

    Update boj 1016

---
+++ boj/math/1016.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+a, b = map(int, input().split())
+check = [1]*(b-a+1)
+
+cnt = 0
+i = 2
+
+while i**2 <= b:
+    mul = a // (i**2)
+    while mul * (i**2) <= b:
+        if 0 <= mul * (i**2) - a <= b-a:
+            check[mul * (i**2) - a] = 0
+        mul += 1
+    i += 1
+
+print(sum(check))
