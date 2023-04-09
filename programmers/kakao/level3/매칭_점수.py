cdccb82e874ee8fc79955b4ac37b45b62275b227
cpwoo <world9969@khu.ac.kr>
2023-04-09 16:20:15 +0900

    Update [매칭_점수]

---
+++ programmers/kakao/level3/매칭_점수.py
@@ -0,0 +1,36 @@
+import re
+
+def getScore(word, page):
+    return re.sub('[^a-z]+', '.', page.lower()).split('.').count(word.lower())
+
+def solution(word, pages):
+    webpages = {}
+    for i, page in enumerate(pages):
+        pagetitle = page.split('<meta property=\"og:url\" content=\"')[1].split('\"')[0]
+        links = []
+        for link_long in page.split('a href=\"')[1:]:
+            links.append(link_long.split('\"')[0])
+        webpages[pagetitle] = [i, getScore(word, page), links, 0]
+
+    for page in webpages.values():
+        for target in page[2]:
+            try:
+                webpages[target][3] += page[1]/len(page[2])
+            except:
+                pass
+
+    answer = []        
+    for page in webpages.values():
+        answer.append([page[0], page[1]+page[3]])
+
+    answer.sort(key=lambda x: (-x[1], x[0]))
+    return answer[0][0]
+
+
+word = "blind"
+pages = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]
+print(solution(word, pages)) # 0
+
+word = "Muzi"
+pages = ["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"]
+print(solution(word, pages)) # 1
