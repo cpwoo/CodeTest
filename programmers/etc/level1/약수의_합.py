7d6a64ceb706685bc41340957b2e57f2f0126fa7
cpwoo <world9969@khu.ac.kr>
2023-04-20 23:58:39 +0900

    Update [약수의_합]

---
+++ programmers/etc/level1/약수의_합.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = 0
+    for i in range(1, n+1):
+        if n%i == 0:
+            answer += i
+    return answer
+
+
+n = 12
+print(solution(n)) # 28
+
+n = 5
+print(solution(n)) # 6
