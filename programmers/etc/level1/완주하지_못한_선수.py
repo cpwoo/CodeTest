951adc7c2362a430a5d7ada73999360d931d7ee7
cpwoo <world9969@khu.ac.kr>
2023-04-21 15:26:03 +0900

    Update [완주하지_못한_선수]

---
+++ programmers/etc/level1/완주하지_못한_선수.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+def solution(participant, completion):
+    answer = Counter(participant) - Counter(completion)
+    return list(answer.keys())[0]
+
+
+participant = ["leo", "kiki", "eden"]
+completion = ["eden", "kiki"]
+print(solution(participant, completion))
+
+participant = ["marina", "josipa", "nikola", "vinko", "filipa"]
+completion = ["josipa", "filipa", "marina", "nikola"]
+print(solution(participant, completion))
+
+participant = ["mislav", "stanko", "mislav", "ana"]
+completion = ["stanko", "ana", "mislav"]
+print(solution(participant, completion))
