c0424df91287dbbdcf6642a1c9abd669d325a6ed
cpwoo <world9969@khu.ac.kr>
2023-05-01 16:14:38 +0900

    Update [숫자_변환하기]

---
+++ programmers/etc/level2/숫자_변환하기.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+def solution(x, y, n):
+    answer = 0
+    q = deque([x])
+    visited = [-1]*(y+1)
+    visited[x] = 0
+    while q:
+        cur = q.popleft()
+        for nxt in [cur+n, cur*2, cur*3]:
+            if nxt <= y and visited[nxt] == -1:
+                visited[nxt] = visited[cur]+1
+                q.append(nxt)
+    return visited[y]
+
+
+x, y, n = 10, 40, 5
+print(solution(x, y, n)) # 2
+
+x, y, n = 10, 40, 30
+print(solution(x, y, n)) # 1
+
+x, y, n = 2, 5, 4
+print(solution(x, y, n)) # -1
