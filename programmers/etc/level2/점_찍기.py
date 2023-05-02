ab48290500aede71cadb0798894b4271092a81b6
cpwoo <world9969@khu.ac.kr>
2023-05-02 18:49:35 +0900

    Update [점_찍기]

---
+++ programmers/etc/level2/점_찍기.py
@@ -0,0 +1,12 @@
+def solution(k, d):
+    answer = 0
+    for i in range(0, d+1, k):
+        answer += int((d**2-i**2)**0.5)//k + 1
+    return answer
+
+
+k, d = 2, 4
+print(solution(k, d)) # 6
+
+k, d = 1, 5
+print(solution(k, d)) # 26
