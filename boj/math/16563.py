5b147730035b59d610a23f98aa2cf941bee078b9
cpwoo <world9969@khu.ac.kr>
2023-06-14 23:59:03 +0900

    Update boj 16563

---
+++ boj/math/16563.py
@@ -0,0 +1,25 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+MAX = 5_000_001
+
+sieve = list(range(MAX))
+sieve[0] = sieve[1] = -1
+for i in range(2, int(MAX**0.5)+1):
+    if sieve[i] == i:
+        for j in range(2*i, MAX, i):
+            if sieve[j] == j:
+                sieve[j] = i
+
+prime = [i for i in range(2, MAX) if sieve[i] == i]
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+for a in arr:
+    ans = []
+    while a > 1:
+        ans.append(sieve[a])
+        a //= sieve[a]
+    print(*ans)
