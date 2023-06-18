d0aee6f489dfa44a5cf3323c8341ccc4ce66892a
cpwoo <world9969@khu.ac.kr>
2023-06-18 19:56:22 +0900

    Modify boj 10217

--- boj/shortestPath/10217.py
+++ boj/shortestPath/10217.py
@@ -3,30 +3,28 @@ input = lambda: sys.stdin.readline().rstrip()
 
 from heapq import *
 
-INF = int(1e9)
+INF = sys.maxsize
+
 for _ in range(int(input())):
-    n, cost, m = map(int, input().split())
-    dp = [[INF]*n for _ in range(cost+1)]
+    ans = INF
+    n, m, k = map(int, input().split())
     graph = [[] for _ in range(n+1)]
-
-    for _ in range(m):
+    for _ in range(k):
         u, v, c, d = map(int, input().split())
-        graph[u-1].append((v-1, c, d))
-     
-    q = [(0, 0, 0)]
-    while q:
-        curDist, curCost, curNode = heappop(q)
-        if curDist > dp[curCost][curNode]:
-            continue
+        graph[u].append((v, c, d))
 
-        for toNode, toCost, toDist in graph[curNode]:
-            d = curDist + toDist
-            c = curCost + toCost
-            if c <= cost and d < dp[c][toNode]:
-                for i in range(c, cost+1):
-                    if dp[i][toNode] > d:
-                        dp[i][toNode] = d
-                    else:
-                        break
-                heappush(q, (d, c, toNode))
-    print(dp[cost][n-1] if dp[cost][n-1] != INF else "Poor KCM")
+    dp = [[INF]*(m+1) for _ in range(n+1)]
+    dp[1][0] = 0
+
+    for c in range(m+1):
+        for d in range(1, n+1):
+            if dp[d][c] == INF:
+                continue
+            t = dp[d][c]
+            for nv, nc, nd in graph[d]:
+                if nc+c > m:
+                    continue
+                dp[nv][nc+c] = min(dp[nv][nc+c], t+nd)
+    
+    res = min(dp[n])
+    print(res if res != INF else "Poor KCM")
