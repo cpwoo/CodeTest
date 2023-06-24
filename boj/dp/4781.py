1d608ade40dc47dfb778e1dff0f2b50deb70f0d6
cpwoo <world9969@khu.ac.kr>
2023-06-24 17:55:34 +0900

    Modify boj 4781

--- boj/dp/4781.py
+++ boj/dp/4781.py
@@ -1,6 +1,7 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
+
 while True:
     n, m = input().split()
     
@@ -9,16 +10,23 @@ while True:
 
     n, m = int(n), int(float(m)*100)
 
-    candies = []
+    tmp = []
     for _ in range(n):
         c, p = input().split()
-        candies.append([int(c), int(float(p)*100+0.5)])
+        tmp.append([int(c), int(float(p)*100+0.5)])
+    tmp.sort()
 
+    candies = [tmp[0]]
+
+    for i in range(1, n):
+        if tmp[i][0] > candies[-1][0]:
+            candies.append(tmp[i])
+    
     dp = [0]*(m+1)
-    for i in range(1, m+1):
-        for j in range(n):
-            c, p = candies[j]
-            if p <= i:
-                dp[i] = max(dp[i], dp[i-p]+c)
+    for c, p in candies:
+        for i in range(p, m+1):
+            cnt = dp[i-p]+c
+            if cnt > dp[i]:
+                dp[i] = cnt
 
     print(dp[m])
