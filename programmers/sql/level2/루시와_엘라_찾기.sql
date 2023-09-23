05753cb93aa3fc109cb39f61384e5267abf745a3
cpwoo <world9969@khu.ac.kr>
2023-09-23 22:39:48 +0900

    Update [루시와_엘라_찾기]

---
+++ programmers/sql/level2/루시와_엘라_찾기.sql
@@ -0,0 +1,11 @@
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM  ANIMAL_INS 
+WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
+ORDER BY ANIMAL_ID;
+
+/* ORACLE
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM  ANIMAL_INS 
+WHERE NAME IN ('Lucy','Ella','Pickle','Rogan','Sabrina','Mitty')
+ORDER BY ANIMAL_ID;
+*/
