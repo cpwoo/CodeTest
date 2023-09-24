d84d67f7ca9a071d3e4485b78f0ea3cf60ac90ec
cpwoo <world9969@khu.ac.kr>
2023-09-24 14:55:20 +0900

    Update [조건별로_분류하여_주문상태_출력하기]

---
+++ programmers/sql/level3/조건별로_분류하여_주문상태_출력하기.sql
@@ -0,0 +1,9 @@
+SELECT 
+    ORDER_ID, 
+    PRODUCT_ID, 
+    DATE_FORMAT(OUT_DATE, '%Y-%m-%d') OUT_DATE,
+    (CASE WHEN OUT_DATE IS NULL THEN '출고미정'
+    WHEN DATE_FORMAT(OUT_DATE, '%m-%d') <= '05-01' THEN '출고완료'
+    ELSE '출고대기' END) 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID;
