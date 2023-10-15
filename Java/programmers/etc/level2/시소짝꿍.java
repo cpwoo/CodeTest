d2b74afe77230af7eaad08f8416416ec35c05af4
cpwoo <world9969@khu.ac.kr>
2023-10-15 22:06:05 +0900

    Update [시소짝꿍]

---
+++ Java/programmers/etc/level2/시소짝꿍.java
@@ -0,0 +1,30 @@
+package CodeTest.Java.programmers.etc.level2;
+
+public class 시소짝꿍 {
+    public long solution(int[] weights) {
+        long answer = 0;
+        long[] arr = new long[1001];
+        
+        for (int w : weights) arr[w]++;
+        
+        for (int i=100; i<1001; i++){
+            if(arr[i] == 0) continue;
+            answer += (arr[i]*(arr[i]-1))/2;
+
+            if((4*i)/3 > 1000) continue;
+            if(i%3 == 0){
+                answer += arr[i]*arr[(4*i)/3];
+            }
+
+            if((3*i)/2 > 1000) continue;
+            if(i%2 == 0){
+                answer += arr[i]*arr[(3*i)/2];
+            }
+            
+            if(2*i >1000) continue;
+            answer += arr[i]*arr[2*i];
+        }
+        
+        return answer;
+    }
+}
