bc4a1e426161ee61976868340689c5b021720525
cpwoo <world9969@naver.com>
2024-02-01 16:41:24 +0900

    Update boj 10836

---
+++ Python/boj/simulation/10836.py
@@ -0,0 +1,17 @@
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+m, n = map(int, input().split())
+grow = [1]*(2*m-1)
+
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    for i in range(a, a+b):
+        grow[i] += 1
+    for i in range(a+b, 2*m-1):
+        grow[i] += 2
+
+s = " ".join(str(x) for x in grow[m:])
+
+for i in range(m-1, -1,  -1):
+    print(grow[i], s)
