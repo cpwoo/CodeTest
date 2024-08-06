81ad47704c71202f81adfd0012b1a2daae71f6c3
cpwoo <world9969@naver.com>
2024-08-06 15:55:35 +0900

    Update boj 9251

---
+++ Java/boj/dp/boj9251.java
@@ -0,0 +1,35 @@
+package CodeTest.Java.boj.dp;
+
+import java.io.*;
+
+public class boj9251 {
+    private static BufferedReader br;
+	private static BufferedWriter bw;
+
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve();
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void solve() throws Exception {
+        String A = br.readLine();
+        String B = br.readLine();
+
+        int[][] dp = new int[A.length()+1][B.length()+1];
+
+        for(int i=1; i<A.length()+1; i++) {
+            for(int j=1; j<B.length()+1; j++) {
+                if(A.charAt(i-1) == B.charAt(j-1)) dp[i][j] = dp[i-1][j-1]+1;
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        bw.write(dp[A.length()][B.length()]+"");
+    }
+
+}
