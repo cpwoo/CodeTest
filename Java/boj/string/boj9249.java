8e27b447df3cf2e7a25a237495793d8e063abe2f
cpwoo <world9969@naver.com>
2024-08-21 13:37:24 +0900

    Update boj 9249

---
+++ Java/boj/string/boj9249.java
@@ -0,0 +1,102 @@
+package CodeTest.Java.boj.string;
+
+import java.io.*;
+import java.util.*;
+
+public class boj9249 {
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringBuilder sb;
+
+    private static int t;
+
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve();
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void solve() throws Exception {
+        String a = br.readLine(), b = br.readLine();
+        int A = a.length();
+
+        String str = a+"1"+b;
+        int n = str.length();
+
+        Integer[] suffix = new Integer[n];
+        for(int i=0; i<n; i++) suffix[i] = i;
+
+        int[] g = new int[n+1];
+        for(int i=0; i<n; i++) g[i] = str.charAt(i);
+        g[n] = -1;
+
+        int[] ng = new int[n+1];
+        ng[suffix[0]] = 0; ng[n] = -1;
+
+        t = 1;
+        while(t < n) {
+            Arrays.sort(suffix, new Comparator<Integer>() {
+			    public int compare(Integer o1, Integer o2) {
+				    if(g[o1] != g[o2]) return g[o1]-g[o2];
+				    return g[Math.min(o1+t, n)]-g[Math.min(o2+t, n)];
+			    }
+            });
+
+            for(int i=1; i<n; i++) {
+                int p = suffix[i-1], q = suffix[i];
+
+                if(g[p] != g[q] || g[Math.min(p+t, n)] != g[Math.min(q+t, n)]) {
+                    ng[q] = ng[p]+1;
+                }
+                else ng[q] = ng[p];
+            }
+
+            if(ng[n-1] == n-1) break;
+
+            t <<= 1;
+            for(int i=0; i<n+1; i++) g[i] = ng[i];
+        }
+
+        int[] LCP = new int[n];
+        int L = 0;
+        for(int i=0; i<n; i++) {
+            int k = g[i];
+            if(k == 0) continue;
+            
+            int p = suffix[k-1];
+
+            while(i+L < n && p+L < n && str.charAt(i+L) == str.charAt(p+L)) L++;
+
+            LCP[k] = L;
+
+            if(L != 0) L--;
+        }
+
+        int[] m = new int[2];
+        for(int i=0; i<n; i++) {
+            int p = (i>0) ? suffix[i-1]+LCP[i]-1 : suffix[n-1]+LCP[i]-1;
+            int q = suffix[i]+LCP[i]-1;
+            if(0 <= p && p < A && A < q && q < n) {
+                if(m[0] < LCP[i] || (m[0] == LCP[i] && m[1] <= i)) {
+                    m[0] = LCP[i]; m[1] = i;
+                }
+            }
+            if(0 <= q && q < A && A < p && p < n) {
+                if(m[0] < LCP[i] || (m[0] == LCP[i] && m[1] <= i)) {
+                    m[0] = LCP[i]; m[1] = i;
+                }
+            }
+        }
+
+        sb = new StringBuilder();
+        sb.append(m[0]).append('\n');
+        sb.append(str.substring(suffix[m[1]], suffix[m[1]]+m[0]));
+
+        bw.write(sb.toString());
+    }
+
+}
