66d23a158f92de26cf4f3c7dbad3f7b999689549
cpwoo <world9969@naver.com>
2024-09-03 17:26:38 +0900

    Update boj 1922

---
+++ Java/boj/minimumSpanningTree/boj1922.java
@@ -0,0 +1,80 @@
+package CodeTest.Java.boj.minimumSpanningTree;
+
+import java.io.*;
+import java.util.*;
+
+public class boj1922 {
+    static class Node implements Comparable<Node> {
+        int u, v, w;
+
+        Node(int u, int v, int w) {
+            this.u = u;
+            this.v = v;
+            this.w = w;
+        }
+
+        @Override
+        public int compareTo(Node node) {
+            if(this.w != node.w) return this.w-node.w;
+            else if(this.u != node.u) return this.u-node.u;
+            return this.v-node.v;
+        }
+    }
+
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+
+    private static int parent[];
+
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        solve();
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static void solve() throws Exception {
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        parent = new int[n+1];
+        for(int i=0; i<n+1; i++) parent[i] = i;
+
+        List<Node> nodes = new ArrayList<>();
+        for(int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            nodes.add(new Node(u, v, w));
+        }
+
+        Collections.sort(nodes);
+
+        int ret = 0;
+
+        for(Node node : nodes) {
+            if(find(node.u) != find(node.v)) {
+                union(node.u, node.v);
+                ret += node.w;
+            }
+        }
+
+        bw.write(ret+"");
+    }
+
+    private static int find(int x) {
+        if(parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    private static void union(int a, int b) {
+        a = find(a); b = find(b);
+        parent[Math.max(a, b)] = Math.min(a, b);
+    }
+
+}
