32f763755c80c18309fc72e087e522507137d80e
cpwoo <world9969@naver.com>
2024-09-14 17:51:01 +0900

    Modify boj 12886

--- Python/boj/graph/12886.py
+++ Python/boj/graph/12886.py
@@ -1,33 +1,40 @@
 import sys
-input = lambda: sys.stdin.readline().rstrip()
+def input(): return sys.stdin.readline().rstrip()
 
-from itertools import permutations
+from collections import deque
 
-def answer(x, y, z, cnt):
-    global ans
-    if x <= 0 and y <= 0 and z <= 0:
-        if ans > cnt:
-            ans = cnt
-        return
-    
-    x = 0 if x <= 0 else x
-    y = 0 if y <= 0 else y
-    z = 0 if z <= 0 else z
+def bfs(a, b, c):
+    total = a+b+c
+    visited = [[False]*(total+1) for _ in range(total+1)]
+    q = deque()
+    q.append([a, b])
+    visited[a][b] = True
+    while q:
+        x, y = q.popleft()
+        z = total - (x+y)
+        if x == y == z:
+            return 1
+        for a, b in (x, y), (y, z), (x, z):
+            if a < b:
+                b -= a
+                a += a
+            elif a > b:
+                a -= b
+                b += b
+            else:
+                continue
+            c = total - (a+b)
+            x = min(a, b, c)
+            y = max(a, b, c)
+            if not visited[x][y]:
+                q.append([x, y])
+                visited[x][y] = True
+    return 0
 
-    if dp[x][y][z] <= cnt and dp[x][y][z] != 0:
-        return
-    
-    dp[x][y][z] = cnt
 
-    for i in permutations([9, 3, 1], 3):
-        answer(x-i[0], y-i[1], z-i[2], cnt+1)
+a, b, c = map(int, input().split())
 
-
-N = int(input())
-a = list(map(int, input().split()))
-while len(a) < 3:
-    a += [0]
-ans = 100
-dp = [[[100]*(max(a)+1) for _ in range(max(a)+1)] for _ in range(max(a)+1)]
-answer(a[0], a[1], a[2], 0)
-print(ans)
+if (a+b+c)%3 != 0:
+    print(0)
+else:
+    print(bfs(a, b, c))
