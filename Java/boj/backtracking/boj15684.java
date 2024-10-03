f5f0e30f314bee1c31030c928e3da1dc8a65a386
cpwoo <world9969@naver.com>
2024-10-03 16:32:17 +0900

    Modify boj 15684

--- Java/boj/backtracking/boj15684.java
+++ Java/boj/backtracking/boj15684.java
@@ -1,36 +1,45 @@
 package CodeTest.Java.boj.backtracking;
 
 import java.io.*;
+import java.util.*;
 
 public class boj15684 {
-    private static int N, H, M, answer;
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringTokenizer st;
+
+    private static int N, M, H, answer;
     private static boolean[][] v;
 
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] inp = br.readLine().split(" ");
-        N = Integer.parseInt(inp[0]);
-        H = Integer.parseInt(inp[1]);
-        M = Integer.parseInt(inp[2]);
+        solve();
 
-        v = new boolean[M][N];
+        bw.flush();
+        bw.close();
+    }
+
+    private static void solve() throws Exception {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+
+        v = new boolean[H][N];
         int a, b;
 
-        for(int i=0; i<H; i++) {
-            inp = br.readLine().split(" ");
-            a = Integer.parseInt(inp[0]);
-            b = Integer.parseInt(inp[1]);
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
             v[a-1][b-1] = true;            
         }
 
         answer = 4;
         dfs(0, 0, 0);
         bw.write(answer <= 3 ? answer+"" : "-1");
-
-        bw.flush();
-        bw.close();
     }
 
     private static void dfs(int cnt, int x, int y) {
@@ -44,7 +53,7 @@ public class boj15684 {
         if(cnt == 3) return;
 
         int k;
-        for(int i=x; i<M; i++) {
+        for(int i=x; i<H; i++) {
             k = (i == x) ? y : 0;
             for(int j=k; j<N-1; j++) {
                 if(!v[i][j]) {
@@ -61,7 +70,7 @@ public class boj15684 {
 
         for(int i=0; i<N; i++) {
             tmp = i;
-            for(int j=0; j<M; j++) {
+            for(int j=0; j<H; j++) {
                 if(v[j][tmp]) {
                     tmp++;
                 } else if(tmp > 0 && v[j][tmp-1]) {
